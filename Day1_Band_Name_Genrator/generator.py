6fc502269a260110d84880b02fc88337cd671871
Andrew Hojnowski <ahojo@upenn.edu>
2021-01-03 04:45:48 +0000

    Completed Python day1

---
+++ Day1_Band_Name_Genrator/generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the band name generator!")
+print("What city did you grow up in?")
+city = input("> ")
+print("What is the name of your pet?")
+pet = input("> ")
+
+print("Your band name is: " + city + "  " + pet)
---
+++ main.py
@@ -0,0 +1,6 @@
+a = input("a:")
+b = input("b: ")
+
+a, b = b, a
+
+print(f"{a}, {b}")
