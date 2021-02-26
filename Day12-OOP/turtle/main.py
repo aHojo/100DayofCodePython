faa2b2fd00449ce1bc55e914bdbc3563c8dcde06
ahojo <ahojo@upenn.edu>
2021-02-26 14:42:25 -0500

    Started OOP

---
+++ Day12-OOP/turtle/main.py
@@ -0,0 +1,14 @@
+from turtle import Screen, Turtle, Screen
+
+timmy = Turtle()
+
+
+print(timmy)
+
+timmy.shape("turtle")
+timmy.color("DarkOrchid")
+timmy.forward(50)
+my_screen = Screen()
+print(my_screen.canvheight)
+
+my_screen.exitonclick()
